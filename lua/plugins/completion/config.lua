f229d8f96e6015779eb2266cf39bae451d080883
jet <xuhalsn@gmail.com>
2022-10-24 23:40:13 +0800

    update

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -48,10 +48,6 @@ function config.cmp()
   local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
   end
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
 
   local border = function(hl)
     return {
@@ -139,8 +135,6 @@ function config.cmp()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif has_words_before() then
-          cmp.complete()
         else
           fallback()
         end
