8aa8316d28cb6489c4f4a50e4a7c361f5e98d4b7
xuhaosen <xuhalsn@gmail.com>
2023-10-24 17:37:07 +0800

    update

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -108,7 +108,7 @@ end
 
 function config.copilot()
   vim.cmd([[
-    imap <silent><script><expr> <S-TAB> copilot#Accept("\<CR>")
+    imap <silent><script><expr> <C-H> copilot#Accept("\<CR>")
     let g:copilot_no_tab_map = v:true
   ]])
 end
